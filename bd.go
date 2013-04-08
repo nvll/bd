1471e846f889d553457b2f96f34a5c803e5f0e0f
Chris Anderson <chris@nullcode.org>
2013-04-07 22:26:02 -0700

    Display working. Still need to sort some length / EOF bugs

--- bd.go
+++ bd.go
@@ -19,8 +19,64 @@ func byteDiffers(offset int, files FileArr) bool {
 	return false
 }
 
-func compareLine(offset int, fileData FileArr) {
+func lineDiffers(offset int, fileData FileArr) (bool, [16]bool) {
+	var differs bool
+	var diffMask [16]bool
+	var target uint8
+
+	for i := 0; i < 16; i++ {
+		if offset + i >= len(fileData[0]) {
+			return differs, diffMask
+		}
+
+		for f := range fileData {
+			if f == 0 {
+				target = fileData[f][offset + i]
+			} else {
+				if fileData[f][offset + i] != target || offset + i > len(fileData[f]) {
+					diffMask[i] = true
+					differs = true
+					break
+				}
+			}
+		}
+	}
+
+	return differs, diffMask
 }
+
+
+func outputHex(offset int, file []uint8) string {
+	if offset >= len(file) {
+		return "  "
+	}
+
+	return fmt.Sprintf("%02x", file[offset])
+}
+
+
+func compareLine(offset int, fileData FileArr) {
+	differs, diffMask := lineDiffers(offset, fileData)
+	_,_ = differs, diffMask
+	fmt.Printf("%08x  ", offset)
+	for f := range fileData {
+		for i := 0; i < 16; i++ {
+			tmp := outputHex(offset + i, fileData[f])
+			if (diffMask[i]) {
+				fmt.Printf("\x1b[31;1m%s\x1b[0m ", tmp)
+			} else {
+				fmt.Printf("%s ", tmp)
+			}
+
+			if i == 7 {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Print("\t")
+	}
+	fmt.Print("\n")
+}
+
 func maxSize(f FileArr) int {
 	var max int
 
